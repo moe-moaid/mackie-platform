15b80362e069c913b6e16446e1a4da61c747632b
moe-moaid <mohammadmoaid@gmail.com>
2022-10-03 22:34:17 +0500

    Complete FrontEnd Template

---
+++ frontend/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".hz-feature-slider").slick({
+    infinite: false,
+    speed: 2000,
+    autoplay: true,
+    fade: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
